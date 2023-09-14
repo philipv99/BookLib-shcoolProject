0fc48241941e9a65a84e4df353b162a12a7d184c
philipv99 <philipv9913@gmail.com>
2023-09-14 18:26:23 +0200

    Add project files.

---
+++ BookLib/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace BookLib
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+
+        public Book(string name, int price)
+        {
+            Id = 0;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"the book: {Name}. cost: {Price}";
+        }
+
+        public void ValidateName()
+        {
+            if (Name == null || Name == string.Empty)
+            {
+                throw new ArgumentNullException("name is null or empty");
+            }
+            if (Name.Count() < 3)
+            {
+                throw new ArgumentException("name of book is to short");
+            }
+        }
+
+        public void ValidatePrice() 
+        {
+            if (Price < 0 || Price >= 1200)
+            {
+                throw new ArgumentException("price is not between 0 & 1200");
+            }
+        }
+    }
+}
---
+++ BookLibTests/BookTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLib.Tests
+{
+    [TestClass()]
+    public class BookTests
+    {
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            Book harryBlobber = new Book("harry Blobber & the white man", 500);
+            Assert.IsNotNull(harryBlobber);
+            Assert.IsInstanceOfType(harryBlobber, typeof(Book));
+        }
+
+        [TestMethod()]
+        public void ValidateNameToShort()
+        {
+            Book harryBlobber = new Book("HP", 500);
+            Assert.ThrowsException<ArgumentException>(() => Book harryBlobber = new Book("HP", 500);)
+        }
+    }
+}
